c1c20e73ba0a4cd1e8460b33711937fb1e6d0cfb
Willian Peixoto <will.s.peixoto@gmail.com>
2016-09-24 23:35:00 -0300

    testeData

--- app/server.js
+++ app/server.js
@@ -38,7 +38,7 @@ router.route('/tags')
                  //console.log(req.body.IdTag);
         tag.IdArduino = req.body.IdArduino;
                 //console.log(req.body.IdArduino);
-        tag.DataLeitura = new Date(Date.now());
+        //tag.DataLeitura = new Date(Date.now());
 
         tag.save(function(err) {
             //console.log('erroUUU');
