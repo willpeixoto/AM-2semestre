e59e78f522b3096284b4bd9121a0e0c5998cecc9
Willian Peixoto <will.s.peixoto@gmail.com>
2016-09-28 19:28:14 -0300

    sem timespam

--- app/models/tag.js
+++ app/models/tag.js
@@ -6,10 +6,6 @@ var TagSchema   = new Schema({
     IdTag: String,
     IdArduino : String,
     DataLeitura :  { type: Date, default: Date.now }
-},
-{
-   timestamps: true
-}
-);
+});
 
 module.exports = mongoose.model('Tag', TagSchema);
